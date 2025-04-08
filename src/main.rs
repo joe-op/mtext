eb95928d13ff4967aa68aa474bfbf3f25d25e755
Joe Opseth <joe.opseth@outlook.com>
2025-04-08 07:48:53 -0500

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use anyhow::Result;
+use clap::Parser;
+
+/// Format a source file using templates
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    /// The source file to process
+    #[arg(short, long)]
+    source: String,
+    /// The directory containing the Tera templates used for formatting
+    #[arg(short, long)]
+    template_directory: String,
+}
 
 fn main() -> Result<()> {
+    let args = Args::parse();
+
     Ok(())
 }
