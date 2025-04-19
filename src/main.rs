8b52aaaefdf38c72f1ffb39c27cf7b936ce9bff3
Joe Opseth <joe.opseth@outlook.com>
2025-04-19 11:12:35 -0500

    Create reader and writer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod process;
 
 use anyhow::Result;
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Stdout, Write};
 
 /// Format a source file using templates
 #[derive(Parser)]
@@ -11,20 +13,31 @@ struct Args {
     #[arg(short, long)]
     source: String,
     /// The output file or stdout, default stdout
-    #[arg(short, long, default_value="-")]
+    #[arg(short, long, default_value = "-")]
     output: String,
     /// The directory containing the Tera templates used for formatting
     #[arg(short, long)]
     template_directory: String,
 }
 
+fn stdout_writer() -> BufWriter<Stdout> {
+    BufWriter::new(std::io::stdout())
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    // file from source
-    // BufReader from file
-    // BufWriter for output
-    // process
+    // TODO: evaluate r/w open strategy
+    let f = File::open(args.source)?;
+    let reader = BufReader::new(f);
+    if args.output == "-" {
+        let writer = BufWriter::new(std::io::stdout());
+        process::process(reader, &writer)?;
+    } else {
+        let f = File::open(args.output)?;
+        let writer = BufWriter::new(f);
+        process::process(reader, &writer);
+    }
 
     Ok(())
 }
--- src/process.rs
+++ src/process.rs
@@ -5,19 +5,23 @@ use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 pub fn process<I, O>(reader: BufReader<I>, writer: &BufWriter<O>) -> Result<()>
 where
     I: Sized + Read,
-    O: ?Sized + Write,
+    O: Write,
 {
     for line in reader.lines() {
         let line = line.with_context(|| "I/O error while reading input")?;
         if !line.trim().is_empty() {
             // TODO: provide debug information
             // TODO: test that debug information is printed
+            // FIXME: whole line is being taken as the template name
             let template_name = line
-                .splitn(1, " ")
-                .into_iter()
+                .splitn(1, ' ')
                 .next()
                 .ok_or(anyhow!("Failed to parse line"))?;
-            println!("{}", template_name);
+            println!("Template: {}", template_name);
+
+            // load template
+            // format using template
+            // write
         }
     }
 
